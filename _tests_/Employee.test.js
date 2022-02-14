1b5d307a95841c7eb164e876762ce6d24625e34d
jclong <j.mail1014@gmail.com>
2022-02-14 12:47:22 -0500

    create emloyee test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,10 +1,35 @@
-const { TestWatcher } = require("jest");
 const Employee = require("../lib/Employee");
 
-TestWatcher("employee object", () => {
+test(" create employee object", () => {
   const employee = new Employee("Jess", 10, "jess@gmail.com");
 
   expect(employee.name).toEqual(expect.any(String));
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
+});
+
+test("get employee name", () => {
+  const employee = new Employee("Jess", 10, "jess@gmail.com");
+
+  expect(employee.getName()).toEqual(expect.amy(String));
+});
+
+test("get employee id", () => {
+  const employee = new Employee("Jess", 10, "jess@gmail.com");
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test("get employee email", () => {
+  const employee = new Employee("Jess", 10, "jess@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaiining(employee.email.toString())
+  );
+});
+
+test("get employee role", () => {
+  const employee = new Employee("Jess", 10, "jess@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
 });
